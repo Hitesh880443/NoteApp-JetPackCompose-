bf81a82b00baa80b2ecfd5b118e238b1936d5fa1
Hitesh Sutar <hsutar00@gmail.com>
2022-10-04 18:58:30 +0530

    first commit

---
+++ app/src/main/java/com/hitesh/jetpackcompose_noteapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.hitesh.jetpackcompose_noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hitesh.jetpackcompose_noteapp.ui.theme.JetpackComposeNoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeNoteAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeNoteAppTheme {
+        Greeting("Android")
+    }
+}
